540cd61836604b8bcf32292f867535f93a6f8af0
agamboar <gamboaalvaro96@gmail.com>
2021-03-01 05:26:22 -0300

    tengo sueño

--- horarios/src/components/HPosibles.js
+++ horarios/src/components/HPosibles.js
@@ -3,10 +3,6 @@ import Navbar from './Navbar'
 import Horarios from './Horarios'
 import axios from 'axios';
 
-
-
-
-
 export default class HPosibles extends Component {
 
     state = {
@@ -15,33 +11,37 @@ export default class HPosibles extends Component {
 
     componentDidMount = async () => {
         const horarios_posibles = await axios.get("http://127.0.0.1:8000/clique/", { headers: { Authorization: "Token 66c54201f64d384caea2e56b2c6eb1bd11952176" } })
+        console.log('CHAUFAN2')
+        console.log(horarios_posibles)
         this.setState({
-            Horarios: horarios_posibles.data
+            Horarios: horarios_posibles
         })
+
     }
 
     render() {
         return (
             <div>
-                <Navbar/>
+                <Navbar />
 
-                <br/>
-                <br/>
-                
+                <br />
+                <br />
+
 
                 <p className="lead">
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     A continuación veras los horarios que te recomendamos tomar para tu semestre actual, esperamos haberte ayudado!
                 </p>
-                <br/>
-                <br/>
-                <Horarios horarios = {this.state.Horarios}/>
-                <br/>
+                <br />
+                <br />
 
-                <br/>
-                
+                <Horarios horarios={this.state.Horarios} />
+                <br />
 
-                
+                <br />
+
+
+
 
 
             </div>
