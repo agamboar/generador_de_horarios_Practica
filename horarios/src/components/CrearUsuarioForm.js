179862b3fcd73db5efeb4237c8abde04cc24e6c8
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-02 19:54:34 -0300

    add axios

--- horarios/src/components/CrearUsuarioForm.js
+++ horarios/src/components/CrearUsuarioForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+
 import jquery from 'jquery'; 
 function getCookie(name) {
   var cookieValue = null;
