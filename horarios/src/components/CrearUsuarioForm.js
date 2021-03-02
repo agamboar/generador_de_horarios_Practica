5364876b8027c903ebe0c54f1da8d4d75e2c0d58
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-02 19:45:44 -0300

    algo

--- horarios/src/components/CrearUsuarioForm.js
+++ horarios/src/components/CrearUsuarioForm.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+          var cookie = jQuery.trim(cookies[i]);
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
+  }
+  return cookieValue;
+}
 export default class CrearUsuarioForm extends Component {
 
   state = {
