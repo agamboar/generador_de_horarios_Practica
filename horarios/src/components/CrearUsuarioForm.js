845d888a471739f7fbb6cdae388996fc5122aae4
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-03 01:53:01 -0300

    aa

--- horarios/src/components/CrearUsuarioForm.js
+++ horarios/src/components/CrearUsuarioForm.js
@@ -65,7 +65,7 @@ export default class CrearUsuarioForm extends Component {
       method: 'post',
       url: 'http://200.14.84.238:443/accounts/signup/',
       headers: {
-        'X-CSRFToken': "csrftoken="+getCsrfToken(), 
+        'X-CSRFToken': "a2PUvrvfQCywbusDq32W7VD2tjN1gTBhzQOvWT4n92egNS8HEfiT00m7vS9ha40s", 
         'Content-Type': 'application/x-www-form-urlencoded',  
       },
       data: data
