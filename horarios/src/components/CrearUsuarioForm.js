deeb445bde0cc4701a99f766c9e61b7dbacb70a7
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-02 23:08:49 -0300

    algo

--- horarios/src/components/CrearUsuarioForm.js
+++ horarios/src/components/CrearUsuarioForm.js
@@ -6,6 +6,7 @@ import $ from 'jquery';
 
 function getCookie(name) {
   var cookieValue = null;
+  console.log(document.cookie)
   if (document.cookie && document.cookie !== '') {
       var cookies = document.cookie.split(';');
       for (var i = 0; i < cookies.length; i++) {
