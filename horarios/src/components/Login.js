d071ca3a09d50ed1656bc4190a4edeede99d1686
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-02 22:52:58 -0300

    cookies

--- horarios/src/components/Login.js
+++ horarios/src/components/Login.js
@@ -2,16 +2,15 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import GoogleLogin from 'react-google-login';
 import googleLogin from "../services/googleLogin"
-
+import $ from 'jquery'; 
 var $ = require('zepto-browserify').$;
 
-var getCookie = function (name) {
+var getCookie(name) {
     var cookieValue = null;
     if (document.cookie && document.cookie !== '') {
         var cookies = document.cookie.split(';');
         for (var i = 0; i < cookies.length; i++) {
-            var cookie = $.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
+            var cookie = jQuery.trim(cookies[i]);
             if (cookie.substring(0, name.length + 1) === (name + '=')) {
                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                 break;
