b75420455d28876a6f69928e606553378dd6e211
Benjamín Morales <benjamin.morales1997@gmail.com>
2021-03-02 21:43:21 -0300

    cambios menores

--- horarios/src/components/Login.js
+++ horarios/src/components/Login.js
@@ -27,10 +27,10 @@ export default class GoogleSocialAuth extends Component {
                     <br />
                     <h5>
                         &nbsp;&nbsp;&nbsp;
-                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="bi bi-envelope" viewBox="0 0 22 22">
-                            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383l-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
                         </svg>
-                E-Mail
+                Usuario
                 </h5>
                     <div className="form-group">
                         <input type="Tel" className="form-control rounded-pill" placeholder="Ej: prueba@mail.cl" name="email" />
@@ -51,7 +51,7 @@ export default class GoogleSocialAuth extends Component {
                         <button type="submit" className="btn btn-primary rounded-pill btn-sm" onClick={this.cambiaestenombre()}>
                             <Link className="nav-link" to={{ pathname: '/users/usr' }} style={{ color: '#FFF' }} >
                                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                Ingresar Con Tú Email
+                                Ingresar Con Tú Usuario
                                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         </Link>
                         </button>
