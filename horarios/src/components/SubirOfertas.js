d3c67ecca3f5236103063b0c3d0419a95d671a74
agamboar <gamboaalvaro96@gmail.com>
2021-03-04 04:22:56 -0300

    upload ofertas

--- horarios/src/components/SubirOfertas.js
+++ horarios/src/components/SubirOfertas.js
@@ -19,157 +19,157 @@ export default class CrearHorario extends Component {
         console.log(e.target.files[0]);
 
     }
-}
 
-addInformatica = async (informatica_excel) => {
 
-    // aqui va el axios
+    addInformatica = async (informatica_excel) => {
 
-    let formdata = new FormData()
-    formdata.append('informatica', this.state.informatica_excel)
+        // aqui va el axios
 
-    var config = {
-        method: 'post',
-        url: 'http://200.14.84.238:443/upload/',
-        headers: {
-            'Content-Type': 'multipart/form-data',
-            'Authorization': 'Token ' + sessionStorage.getItem("token")
-        },
-        data: formdata
+        let formdata = new FormData()
+        formdata.append('informatica', this.state.informatica_excel)
+
+        var config = {
+            method: 'post',
+            url: 'http://200.14.84.238:443/upload/',
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                'Authorization': 'Token ' + sessionStorage.getItem("token")
+            },
+            data: formdata
+        };
+
+        await axios(config)
+
+
     };
 
-    await axios(config)
+    addCfg = async (cfg) => {
+        const newCfg = {
+            cfg: cfg
 
-
-}
-
-addCfg = async (cfg) => {
-    const newCfg = {
-        cfg: cfg
+        }
+        // aqui va el axios
 
     }
-    // aqui va el axios
-
-}
 
 
-onSubmit1 = e => {
-    e.preventDefault();
-    this.addInformatica(this.state.informatica)
+    onSubmit1 = e => {
+        e.preventDefault();
+        this.addInformatica(this.state.informatica)
 
-}
+    }
 
-onSubmit2 = e => {
-    e.preventDefault();
-    this.addCfg(this.state.cfg)
+    onSubmit2 = e => {
+        e.preventDefault();
+        this.addCfg(this.state.cfg)
 
-}
+    }
 
-render() {
-    return (
-        <div>
-            <Navbar />
+    render() {
+        return (
+            <div>
+                <Navbar />
 
-            <br />
-            <br />
-            <br />
-            <br />
+                <br />
+                <br />
+                <br />
+                <br />
 
-            <div className="container">
+                <div className="container">
 
-                <div className="row">
-                    <div className="col-sm-2 " />
+                    <div className="row">
+                        <div className="col-sm-2 " />
 
-                    <div className="col-sm-4 ">
-                        <div className="card ">
-                            <div className="container">
-                                <br />
-                                <br />
-                                <h1 className="title text-primary text-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" fill="currentColor" className="bi bi-file-excel" viewBox="0 0 16 16">
-                                        <path d="M5.18 4.616a.5.5 0 0 1 .704.064L8 7.219l2.116-2.54a.5.5 0 1 1 .768.641L8.651 8l2.233 2.68a.5.5 0 0 1-.768.64L8 8.781l-2.116 2.54a.5.5 0 0 1-.768-.641L7.349 8 5.116 5.32a.5.5 0 0 1 .064-.704z" />
-                                        <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z" />
-                                    </svg>
-                                </h1>
-                                <br />
-
-                                <div className="card-body">
-                                    <h5 className="card-title text-center">Ingreso de Oferta Académica Informática</h5>
+                        <div className="col-sm-4 ">
+                            <div className="card ">
+                                <div className="container">
                                     <br />
                                     <br />
+                                    <h1 className="title text-primary text-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" fill="currentColor" className="bi bi-file-excel" viewBox="0 0 16 16">
+                                            <path d="M5.18 4.616a.5.5 0 0 1 .704.064L8 7.219l2.116-2.54a.5.5 0 1 1 .768.641L8.651 8l2.233 2.68a.5.5 0 0 1-.768.64L8 8.781l-2.116 2.54a.5.5 0 0 1-.768-.641L7.349 8 5.116 5.32a.5.5 0 0 1 .064-.704z" />
+                                            <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z" />
+                                        </svg>
+                                    </h1>
                                     <br />
-                                    <br />
-                                    <br />
-                                    <br />
-                                    <div className="form-group">
-                                        <input className="form-control form-control-sm"
-                                            type="file"
-                                            name="informatica"
-                                            id="formFile"
-                                            onChange={this.onChange}
-                                            value={this.state.informatica}
-                                            multiple />
+
+                                    <div className="card-body">
+                                        <h5 className="card-title text-center">Ingreso de Oferta Académica Informática</h5>
+                                        <br />
+                                        <br />
+                                        <br />
+                                        <br />
+                                        <br />
+                                        <br />
+                                        <div className="form-group">
+                                            <input className="form-control form-control-sm"
+                                                type="file"
+                                                name="informatica"
+                                                id="formFile"
+                                                onChange={this.onChange}
+                                                value={this.state.informatica}
+                                                multiple />
+                                        </div>
+                                        <form onSubmit={this.onSubmit1}>
+                                            <button type="submit" className="btn btn-primary" >Subir Oferta Informática</button>
+                                        </form>
                                     </div>
-                                    <form onSubmit={this.onSubmit1}>
-                                        <button type="submit" className="btn btn-primary" >Subir Oferta Informática</button>
-                                    </form>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+
+
+                        <div className="col-sm-4 ">
+                            <div className="card ">
+                                <div className="container">
+                                    <br />
+                                    <br />
+                                    <h1 className="title text-primary text-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" fill="currentColor" className="bi bi-file-excel" viewBox="0 0 16 16">
+                                            <path d="M5.18 4.616a.5.5 0 0 1 .704.064L8 7.219l2.116-2.54a.5.5 0 1 1 .768.641L8.651 8l2.233 2.68a.5.5 0 0 1-.768.64L8 8.781l-2.116 2.54a.5.5 0 0 1-.768-.641L7.349 8 5.116 5.32a.5.5 0 0 1 .064-.704z" />
+                                            <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z" />
+                                        </svg>
+                                    </h1>
+                                    <br />
+
+                                    <div className="card-body">
+                                        <h5 className="card-title text-center">Ingreso Oferta Académica CFG</h5>
+                                        <br />
+                                        <br />
+                                        <br />
+                                        <br />
+                                        <br />
+                                        <br />
+                                        <br />
+                                        <div className="form-group">
+                                            <input
+                                                className="form-control form-control-sm"
+                                                type="file"
+                                                name="cfg"
+                                                id="formFile"
+                                                onChange={this.onChange}
+                                                value={this.state.cfg}
+                                                multiple />
+                                        </div>
+                                        <form onSubmit={this.onSubmit2}>
+                                            <button type="submit" className="btn btn-primary">Subir Oferta CFG</button>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
 
                         </div>
 
+
+                        <div className="col-sm-2 " />
                     </div>
 
-
-
-                    <div className="col-sm-4 ">
-                        <div className="card ">
-                            <div className="container">
-                                <br />
-                                <br />
-                                <h1 className="title text-primary text-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" fill="currentColor" className="bi bi-file-excel" viewBox="0 0 16 16">
-                                        <path d="M5.18 4.616a.5.5 0 0 1 .704.064L8 7.219l2.116-2.54a.5.5 0 1 1 .768.641L8.651 8l2.233 2.68a.5.5 0 0 1-.768.64L8 8.781l-2.116 2.54a.5.5 0 0 1-.768-.641L7.349 8 5.116 5.32a.5.5 0 0 1 .064-.704z" />
-                                        <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z" />
-                                    </svg>
-                                </h1>
-                                <br />
-
-                                <div className="card-body">
-                                    <h5 className="card-title text-center">Ingreso Oferta Académica CFG</h5>
-                                    <br />
-                                    <br />
-                                    <br />
-                                    <br />
-                                    <br />
-                                    <br />
-                                    <br />
-                                    <div className="form-group">
-                                        <input
-                                            className="form-control form-control-sm"
-                                            type="file"
-                                            name="cfg"
-                                            id="formFile"
-                                            onChange={this.onChange}
-                                            value={this.state.cfg}
-                                            multiple />
-                                    </div>
-                                    <form onSubmit={this.onSubmit2}>
-                                        <button type="submit" className="btn btn-primary">Subir Oferta CFG</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-
-
-                    <div className="col-sm-2 " />
                 </div>
-
+                <Derechos />
             </div>
-            <Derechos />
-        </div>
-    )
-}
+        )
+    }
 }
