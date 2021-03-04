65243602f088694ec8effce360a67e937e235e51
Benjamín Morales <benjamin.morales1997@gmail.com>
2021-03-04 00:40:33 -0300

    colorear el pert

--- horarios/src/components/RamoC.js
+++ horarios/src/components/RamoC.js
@@ -4,15 +4,35 @@ import React, { Component } from 'react'
 
 export default class Ramo extends Component {
     render() {
-        if(this.props.state[0]===true){
-        return (
-            <div className="col" >  
-                <div className="card border-primary text-center custom" style={{background: '#FF3E17' }}  >                  
-                    <h6 className="card-title"><font size="2">{this.props.codigo}</font></h6>
-                    <p className="card-text"><font size="2">{this.props.ramo}</font></p> 
-                </div>
-            </div>    
+        if(this.props.state===null){
+            return (
+                <div className="col" >  
+                {console.log(this.props)}
+                    <div className="card border-primary text-center custom" style={{background: '#28B463' }} >                  
+                        <h6 className="card-title"><font size="2">{this.props.codigo}</font></h6>
+                        <p className="card-text"><font size="2">{this.props.ramo}</font></p> 
+                    </div>
+                </div>    
+            ) 
+        }else if(this.props.state[0]===true){
+            return (
+                <div className="col" >  
+                    <div className="card border-primary text-center custom" style={{background: '#FF3E17' }}  >                  
+                        <h6 className="card-title"><font size="2">{this.props.codigo}</font></h6>
+                        <p className="card-text"><font size="2">{this.props.ramo}</font></p> 
+                    </div>
+                </div>    
         )}else if(this.props.state[0]===false){
+            if(this.props.state[1]===1){
+            return (
+                <div className="col" >  
+                    <div className="card border-primary text-center custom"  style={{background: '#FFD44F' }} >                  
+                        <h6 className="card-title"><font size="2">{this.props.codigo}</font></h6>
+                        <p className="card-text"><font size="2">{this.props.ramo}</font></p> 
+                    </div>
+                </div>    
+            ) 
+        }else{
             return (
                 <div className="col" >  
                     <div className="card border-primary text-center custom"   >                  
@@ -20,17 +40,9 @@ export default class Ramo extends Component {
                         <p className="card-text"><font size="2">{this.props.ramo}</font></p> 
                     </div>
                 </div>    
-            ) }else{
-                return (
-                    <div className="col" >  
-                    {console.log(this.props)}
-                        <div className="card border-primary text-center custom" style={{background: '#28B463' }} >                  
-                            <h6 className="card-title"><font size="2">{this.props.codigo}</font></h6>
-                            <p className="card-text"><font size="2">{this.props.ramo}</font></p> 
-                        </div>
-                    </div>    
-                )  
-            } 
+            ) 
+        }
+            }
         }
     }
 
