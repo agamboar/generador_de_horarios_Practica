20f4a9be327d18bdb48e1061172b9dea9f679555
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-04 02:43:25 -0300

    fix save pert in avance_academico_json

--- aplicacion_web/TdR/generador_horarios/views.py
+++ aplicacion_web/TdR/generador_horarios/views.py
@@ -207,7 +207,7 @@ def get_PERT(request):
 
             serializer = nodoAsignaturaSerializer(ramos_disponibles, many=True)
 
-            avance_academico_user.json_avance= {"prueba2":"no"} 
+            avance_academico_user.json_avance= serializer.data
             avance_academico_user.save()
         else:
             pass
