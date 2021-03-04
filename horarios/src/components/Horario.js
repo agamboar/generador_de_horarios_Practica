cfe8f7ff804b8eb984463bbdb6d7d551af33cda7
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-04 07:49:37 -0300

    clique front

--- horarios/src/components/Horario.js
+++ horarios/src/components/Horario.js
@@ -222,7 +222,7 @@ export default class Horario extends Component {
 
                         <h5>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ramos:</h5>
                         <div className="row row-cols-2">
-                            
+                            {console.log(this.props.horario)}
                             <RamosH ramos={this.props.horario} />
 
 
