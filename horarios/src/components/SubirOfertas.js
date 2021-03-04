658e826f5861bea53ececd8ad576cfb6708b9612
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-04 05:10:38 -0300

    trying to upload excel inf

--- horarios/src/components/SubirOfertas.js
+++ horarios/src/components/SubirOfertas.js
@@ -7,60 +7,26 @@ import axios from 'axios';
 
 export default class CrearHorario extends Component {
 
-    state = {
-        informatica: null,
-        cfg: null
-    }
 
-    onChange = e => {
-        /*this.setState({
-            [e.target.name]: e.target.value
-        })*/
-        console.log(e.target.files[0]);
+    constructor(props) {
+        super(props);
+          this.state = {
+            selectedFile: null
+          }
+       
+    }
+    onChangeHandler=event=>{
         this.setState({
-            informatica: e.target.files[0],
-          })
-
+          selectedFile: event.target.files[0],
+          loaded: 0,
+        })
     }
-
-    addInformatica = async (informatica_excel) => {
-        console.log(informatica_excel)
-        await axios.post("http://200.14.84.238:443/upload/", informatica_excel).then(res => { 
-            console.log(res.statusText)
-         })
-   
-
-
-    }
-
-    addCfg = async (cfg) => {
-        const newCfg = {
-            cfg: cfg
-
-        }
-        // aqui va el axios
-
-    }
-
-
-    onSubmit1 = e => {
-        e.preventDefault();
-        this.addInformatica(this.state.informatica)
-
-    }
-
-    onSubmit2 = e => {
-        e.preventDefault();
-        this.addCfg(this.state.cfg)
-
-    }
-
     render() {
         return (
             <div>
                 <Navbar />
 
-               
+                <input type="file" name="file" onChange={this.onChangeHandler}/>
                 <Derechos />
             </div>
         )
