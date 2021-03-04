2fed608253e1cb8faaa9c9cb3afb397433ecdca6
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-04 03:54:47 -0300

    alert to redirect

--- horarios/src/components/AvanceManual2018.js
+++ horarios/src/components/AvanceManual2018.js
@@ -37,7 +37,8 @@ export default class AvanceManual2018 extends Component {
         };
 
         axios(config)
-
+        setTimeout(function(){alert("Seras redirigido para calcular tus ramos críticos"); }, 1000); //cambia esto por toast
+        setTimeout(function(){ window.location.href = 'http://200.14.84.238:443/users/usr/PERT'; }, 5000);
     }
 
     render() {
