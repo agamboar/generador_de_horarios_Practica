e050a16c7362fdc7eea19ab34c31a86ad403036e
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-04 05:13:40 -0300

    trying to upload excel inf

--- horarios/src/components/SubirOfertas.js
+++ horarios/src/components/SubirOfertas.js
@@ -15,11 +15,14 @@ export default class CrearHorario extends Component {
           }
        
     }
-    onChangeHandler=event=>{
-        this.setState({
-          selectedFile: event.target.files[0],
-          loaded: 0,
-        })
+    onClickHandler = () => {
+        const data = new FormData()
+        data.append('file', this.state.selectedFile)
+        axios.post("http://localhost:8000/upload", data, { 
+           // receive two    parameter endpoint url ,form data
+       }).then(res => { // then print response status
+        console.log(res.statusText)
+     })
     }
     render() {
         return (
@@ -27,6 +30,7 @@ export default class CrearHorario extends Component {
                 <Navbar />
 
                 <input type="file" name="file" onChange={this.onChangeHandler}/>
+                <button type="button" class="btn btn-success btn-block" onClick={this.onClickHandler}>Upload</button>
                 <Derechos />
             </div>
         )
