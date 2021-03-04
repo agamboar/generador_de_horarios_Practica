5b497f534012afaf00fa0fb92b9713b618458478
agamboar <gamboaalvaro96@gmail.com>
2021-03-04 02:26:00 -0300

    upload ofertas

--- horarios/src/components/SubirOfertas.js
+++ horarios/src/components/SubirOfertas.js
@@ -35,7 +35,7 @@ export default class CrearHorario extends Component {
             method: 'post',
             url: 'http://200.14.84.238:443/upload/',
             headers: {
-                'Content-Type': 'multipart/form-data',
+
                 'Authorization': 'Token ' + sessionStorage.getItem("token"),
                 'Cookie': 'csrftoken=' + sessionStorage.getItem("token"),
                 'X-CSRFTOKEN': sessionStorage.getItem("token")  //cambiiar a localStorage
@@ -89,20 +89,20 @@ export default class CrearHorario extends Component {
 
                     <div className="row">
                         <div className="col-sm-2 " />
-                        
-                            <div className="col-sm-4 ">
-                                <div className="card ">
-                                    <div className="container">
-                                        <br />
-                                        <br />
-                                        <h1 className="title text-primary text-center">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" fill="currentColor" className="bi bi-file-excel" viewBox="0 0 16 16">
-                                                <path d="M5.18 4.616a.5.5 0 0 1 .704.064L8 7.219l2.116-2.54a.5.5 0 1 1 .768.641L8.651 8l2.233 2.68a.5.5 0 0 1-.768.64L8 8.781l-2.116 2.54a.5.5 0 0 1-.768-.641L7.349 8 5.116 5.32a.5.5 0 0 1 .064-.704z" />
-                                                <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z" />
-                                            </svg>
-                                        </h1>
-                                        <br />
-                                    
+
+                        <div className="col-sm-4 ">
+                            <div className="card ">
+                                <div className="container">
+                                    <br />
+                                    <br />
+                                    <h1 className="title text-primary text-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" fill="currentColor" className="bi bi-file-excel" viewBox="0 0 16 16">
+                                            <path d="M5.18 4.616a.5.5 0 0 1 .704.064L8 7.219l2.116-2.54a.5.5 0 1 1 .768.641L8.651 8l2.233 2.68a.5.5 0 0 1-.768.64L8 8.781l-2.116 2.54a.5.5 0 0 1-.768-.641L7.349 8 5.116 5.32a.5.5 0 0 1 .064-.704z" />
+                                            <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z" />
+                                        </svg>
+                                    </h1>
+                                    <br />
+
                                     <div className="card-body">
                                         <h5 className="card-title text-center">Ingreso de Oferta Académica Informática</h5>
                                         <br />
@@ -121,30 +121,30 @@ export default class CrearHorario extends Component {
                                                 multiple />
                                         </div>
                                         <form onSubmit={this.onSubmit1}>
-                                        <button type="submit" className="btn btn-primary" onClick={(e) => this.addInformatica(e)}>Subir Oferta Informática</button>
+                                            <button type="submit" className="btn btn-primary" onClick={(e) => this.addInformatica(e)}>Subir Oferta Informática</button>
                                         </form>
                                     </div>
-                                    </div>
-
                                 </div>
 
                             </div>
 
-                        
-                        
-                            <div className="col-sm-4 ">
-                                <div className="card ">
-                                    <div className="container">
-                                        <br />
-                                        <br />
-                                        <h1 className="title text-primary text-center">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" fill="currentColor" className="bi bi-file-excel" viewBox="0 0 16 16">
-                                                <path d="M5.18 4.616a.5.5 0 0 1 .704.064L8 7.219l2.116-2.54a.5.5 0 1 1 .768.641L8.651 8l2.233 2.68a.5.5 0 0 1-.768.64L8 8.781l-2.116 2.54a.5.5 0 0 1-.768-.641L7.349 8 5.116 5.32a.5.5 0 0 1 .064-.704z" />
-                                                <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z" />
-                                            </svg>
-                                        </h1>
-                                        <br />
-                                    
+                        </div>
+
+
+
+                        <div className="col-sm-4 ">
+                            <div className="card ">
+                                <div className="container">
+                                    <br />
+                                    <br />
+                                    <h1 className="title text-primary text-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" fill="currentColor" className="bi bi-file-excel" viewBox="0 0 16 16">
+                                            <path d="M5.18 4.616a.5.5 0 0 1 .704.064L8 7.219l2.116-2.54a.5.5 0 1 1 .768.641L8.651 8l2.233 2.68a.5.5 0 0 1-.768.64L8 8.781l-2.116 2.54a.5.5 0 0 1-.768-.641L7.349 8 5.116 5.32a.5.5 0 0 1 .064-.704z" />
+                                            <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z" />
+                                        </svg>
+                                    </h1>
+                                    <br />
+
                                     <div className="card-body">
                                         <h5 className="card-title text-center">Ingreso Oferta Académica CFG</h5>
                                         <br />
@@ -165,15 +165,15 @@ export default class CrearHorario extends Component {
                                                 multiple />
                                         </div>
                                         <form onSubmit={this.onSubmit2}>
-                                        <button type="submit" className="btn btn-primary">Subir Oferta CFG</button>
+                                            <button type="submit" className="btn btn-primary">Subir Oferta CFG</button>
                                         </form>
                                     </div>
-                                    </div>
                                 </div>
-
                             </div>
 
-                       
+                        </div>
+
+
                         <div className="col-sm-2 " />
                     </div>
 
