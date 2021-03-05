02054c4b8a3fc77a5b9ef2fdaf66dd57d430b5be
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-05 04:22:57 -0300

    get name pert

--- aplicacion_web/TdR/generador_horarios/serializers.py
+++ aplicacion_web/TdR/generador_horarios/serializers.py
@@ -43,12 +43,12 @@ class nodoAsignaturaSerializer(serializers.ModelSerializer):
         fields = ['holgura', 'ef', 'es', 'ls', 'kk',
                   'lf', 'critico', 'to_asignatura_real','nombre_asignatura']
 
-     def get_nombre_asignatura(self, obj):
-        try:
-            nombre_asignatura = asignatura_real.objects.get(codigo=obj.to_asignatura_real).nombre
-            return nombre_asignatura
-        except  Exception as e:
-            return {}
+     #def get_nombre_asignatura(self, obj):
+     #   try:
+     #       nombre_asignatura = asignatura_real.objects.get(codigo=obj.to_asignatura_real).nombre
+     #       return nombre_asignatura
+     #   except  Exception as e:
+     #       return {}
 
 
 class nodoSeccionSerializer(serializers.ModelSerializer):
