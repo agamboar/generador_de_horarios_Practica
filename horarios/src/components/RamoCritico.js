de2c14fdbe84c33ccefbd581215d00612d793b5e
Benjamín Morales <benjamin.morales1997@gmail.com>
2021-03-04 23:47:09 -0300

    priorizar ramos

--- horarios/src/components/RamoCritico.js
+++ horarios/src/components/RamoCritico.js
@@ -48,8 +48,8 @@ export default class RamoCritico extends Component {
           </Card.Header>
           <Accordion.Collapse eventKey="0">
             <Card.Body>            
-            {console.log(this.props.resultado)}  
-            {console.log(this.props.resultado[0])}
+            {console.log(this.props.resultado, "primero")}  
+            {console.log(this.props.resultado[0], "segundo")}
             <RamoPrioridad ramo={this.props.resultado[0]} onChangeDOWN={this.onChange1_2}/>
             <RamoPrioridad ramo={this.props.resultado[1]} onChangeDOWN={this.onChange2_3} onChangeUP={this.onChange1_2} />
             <RamoPrioridad ramo={this.props.resultado[2]} onChangeDOWN={this.onChange3_4} onChangeUP={this.onChange2_3} />
