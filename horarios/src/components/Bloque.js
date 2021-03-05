f1fcb0038d9324014143d139a8cf36a94590a20b
Benjamín Morales <benjamin.morales1997@gmail.com>
2021-03-05 14:48:26 -0300

    cambios envio de datos

--- horarios/src/components/Bloque.js
+++ horarios/src/components/Bloque.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react'
 
 export default class Bloque extends Component {
     render() {
+        this.forceUpdate();
         console.log(this.props)
         if(this.props.ramo === null){
         return (
