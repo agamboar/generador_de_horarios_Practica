4ff7f492c1bf9429a0096411ae9f7cfa0e4d87ce
Benjamín Morales <benjamin.morales1997@gmail.com>
2021-03-05 14:39:34 -0300

    cambios envio de datos

--- horarios/src/components/Horario.js
+++ horarios/src/components/Horario.js
@@ -69,7 +69,7 @@ export default class Horario extends Component {
                 const mov = i;
                 for (let j = 0; j < this.props.horario[mov].eventos.length; j++) {
                     const mov3 =this.props.horario[i].eventos[j].bloque;
-                    console.log(this.state.horario[i],"hola")
+                    console.log(this.state.horario,"hola")
                     this.setState({
                         [mov3]: this.props.horario[i]
     
