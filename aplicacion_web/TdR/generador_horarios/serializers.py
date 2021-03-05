cb3b8e156cf10ca8798b71515fd4b4110185e034
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-05 04:30:05 -0300

    get name pert

--- aplicacion_web/TdR/generador_horarios/serializers.py
+++ aplicacion_web/TdR/generador_horarios/serializers.py
@@ -35,20 +35,19 @@ class eventoSerializer(serializers.ModelSerializer):
 
 class nodoAsignaturaSerializer(serializers.ModelSerializer):
     
-    nombre_asignatura = asignaturaSerializer("get_nombre")
+    nombre_asignatura = serializers.SerializerMethodField()
 
     class Meta:
         model = nodo_asignatura
         #nombre = generador_horarios.asignatura_real.objects.get(codigo='to_asignatura_real').nombre
-        fields = ['holgura', 'ef', 'es', 'ls', 'kk',
-                  'lf', 'critico', 'to_asignatura_real','nombre_asignatura']
+        fields = ['holgura', 'ef', 'es', 'ls', 'kk', 'lf', 'critico', 'to_asignatura_real','nombre_asignatura']
 
-     #def get_nombre_asignatura(self, obj):
-     #   try:
-     #       nombre_asignatura = asignatura_real.objects.get(codigo=obj.to_asignatura_real).nombre
-     #       return nombre_asignatura
-     #   except  Exception as e:
-     #       return {}
+    def get_nombre_asignatura(self, obj):
+        try:
+            nombre_asignatura = asignatura_real.objects.get(codigo=obj.to_asignatura_real).nombre
+            return nombre_asignatura
+        except  Exception as e:
+            return {}
 
 
 class nodoSeccionSerializer(serializers.ModelSerializer):
