e986e1331baf052993997713643c4604739f57c7
Benjamín Morales <benjamin.morales1997@gmail.com>
2021-03-07 20:58:54 -0300

    cambios esteticos

--- horarios/src/components/PERTE1-2018.js
+++ horarios/src/components/PERTE1-2018.js
@@ -4,7 +4,6 @@ import Semestre from './Semestre'
 import {Link} from 'react-router-dom';
 
 
-
 export default class Malla2018Extra1 extends Component {
 
     state = {
@@ -66,7 +65,7 @@ export default class Malla2018Extra1 extends Component {
 
                 <div className="row row-cols-10">
                     <div className="col col-md-1"> 
-                        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" className="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
                             <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
                         </svg>
                     </div> 
@@ -75,7 +74,7 @@ export default class Malla2018Extra1 extends Component {
                     <ARamo codigo={"CBF1001"} ramo={"Calor y Ondas"} state={this.state.CBF1001} /> 
                     <div className="col col-md-1"> 
                         <Link className="nav-link" to={{ pathname: '/users/usr/PERT/PERTExtra2'}} >
-                            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" className="bi bi-arrow-right-circle" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
                             </svg>
                         </Link>
