822809e1aef5514b4949dba42b5995d33e1b7d26
Benjamín Morales <benjamin.morales1997@gmail.com>
2021-03-07 20:49:36 -0300

    cambios esteticos

--- horarios/src/components/RamoPrioridad.js
+++ horarios/src/components/RamoPrioridad.js
@@ -12,7 +12,7 @@ export default class RamoPrioridad extends Component {
               <div className = 'col-9 '><h5> Ramo: {this.props.ramo.to_asignatura_real[0].nombre} </h5> Código: {this.props.ramo.to_asignatura_real[0].codigo}</div>
               <div className = 'col-auto'> 
                 <div type="button" className="btn" onClick = {this.props.onChangeUP}>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-caret-up" viewBox="0 0 16 16">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" className="bi bi-caret-up" viewBox="0 0 16 16">
                     <path d="M3.204 11h9.592L8 5.519 3.204 11zm-.753-.659l4.796-5.48a1 1 0 0 1 1.506 0l4.796 5.48c.566.647.106 1.659-.753 1.659H3.204a1 1 0 0 1-.753-1.659z"/>
                   </svg>
                 </div>
@@ -21,7 +21,7 @@ export default class RamoPrioridad extends Component {
               <div className = 'col-8'></div>
               <div className = 'col-auto'>
               <div type="button" className="btn" onClick = {this.props.onChangeDOWN}>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-caret-down" viewBox="0 0 16 16">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" className="bi bi-caret-down" viewBox="0 0 16 16">
                     <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659l4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/>
                   </svg>
                 </div>
