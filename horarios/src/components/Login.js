f198f1df4c61aa67b34cc4167ae4c24e98edac09
agamboar <gamboaalvaro96@gmail.com>
2021-03-10 00:22:29 -0300

    fix login

--- horarios/src/components/Login.js
+++ horarios/src/components/Login.js
@@ -46,25 +46,30 @@ export default class GoogleSocialAuth extends Component {
             data: data
         };
 
-        await axios(config).then(response => localStorage.setItem('token', response.data.key))
+        await axios(config).then(response => localStorage.setItem('token', response.data.key)).catch(function (error) {
+            if (error.response) {
+
+                console.log(error.response);
+            }
+        });
 
         if (localStorage.getItem("token")) {
 
             console.log(localStorage.getItem("token"))
             //console.log(localStorage.getItem("user_id"))
-            
+
             var config = {
                 method: 'get',
                 url: 'http://200.14.84.238:80/is_staff/',
                 headers: {
-                    'Authorization': 'Token ' + localStorage.getItem("token"), 
+                    'Authorization': 'Token ' + localStorage.getItem("token"),
                     'Content-Type': 'application/json'
                 }
             };
-    
-            await axios(config).then(response =>   localStorage.setItem('is_staff', response.data.is_staff))
+
+            await axios(config).then(response => localStorage.setItem('is_staff', response.data.is_staff))
             window.location.href = '/users/usr'
-            }
+        }
 
 
     }
@@ -90,52 +95,52 @@ export default class GoogleSocialAuth extends Component {
             console.log(responseGoogle.data)
         }*/
 
-        if (!localStorage.getItem("token")) { 
-        return (
-            <div className="col-md-4 offset-md-4 align-self-sm-end">
-                <div className="card card-body border border-primary shadow-lg p-3 mb-5 bg-white rounded">
-                    <h4 className=" align-self-center">Inicio de Sesión</h4>
-                    <br />
-                    <h5>
-                        &nbsp;&nbsp;&nbsp;
+        if (!localStorage.getItem("token")) {
+            return (
+                <div className="col-md-4 offset-md-4 align-self-sm-end">
+                    <div className="card card-body border border-primary shadow-lg p-3 mb-5 bg-white rounded">
+                        <h4 className=" align-self-center">Inicio de Sesión</h4>
+                        <br />
+                        <h5>
+                            &nbsp;&nbsp;&nbsp;
                         <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
-                        </svg>
+                                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+                            </svg>
                 Usuario
                 </h5>
-                    <form onSubmit={this.onSubmit}>
-                        <div className="form-group">
-                            <input
-                                type="Tel"
-                                name="user"
-                                className="form-control rounded-pill"
-                                placeholder="Ej: usuario"
-                                onChange={this.onChange}
-                                value={this.state.user}
-                            />
-                        </div>
-                        <h5>
-                            &nbsp;&nbsp;
+                        <form onSubmit={this.onSubmit}>
+                            <div className="form-group">
+                                <input
+                                    type="Tel"
+                                    name="user"
+                                    className="form-control rounded-pill"
+                                    placeholder="Ej: usuario"
+                                    onChange={this.onChange}
+                                    value={this.state.user}
+                                />
+                            </div>
+                            <h5>
+                                &nbsp;&nbsp;
                 <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="bi bi-lock" viewBox="0 0 22 22">
-                                <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z" />
-                            </svg>
+                                    <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z" />
+                                </svg>
                 Password
                 </h5>
-                        <div className="form-group">
-                            <input
-                                type="password"
-                                className="form-control rounded-pill"
-                                placeholder="********"
-                                name="password"
-                                onChange={this.onChange}
-                                value={this.state.password}
-                            />
-                        </div>
+                            <div className="form-group">
+                                <input
+                                    type="password"
+                                    className="form-control rounded-pill"
+                                    placeholder="********"
+                                    name="password"
+                                    onChange={this.onChange}
+                                    value={this.state.password}
+                                />
+                            </div>
 
-                        <button type="submit" className="btn btn-outline-primary rounded-pill"> Ingresar
+                            <button type="submit" className="btn btn-outline-primary rounded-pill"> Ingresar
                         </button>
-                    </form>
-                    {/*<p className="lead align-self-center"> o </p>
+                        </form>
+                        {/*<p className="lead align-self-center"> o </p>
                     <div className="App">
                         <h1> Login con Google </h1>
 
@@ -149,16 +154,16 @@ export default class GoogleSocialAuth extends Component {
                         />
 
                     </div>*/}
-                    <br />
-                    <div className=" align-self-end">
-                        <Link className="nav-link" to={{ pathname: '/Registro' }} >Registrarse </Link>
+                        <br />
+                        <div className=" align-self-end">
+                            <Link className="nav-link" to={{ pathname: '/Registro' }} >Registrarse </Link>
+                        </div>
                     </div>
                 </div>
-            </div>
-        )
-    }else{
-        window.location.href = '/users/usr'
-    }
+            )
+        } else {
+            window.location.href = '/users/usr'
+        }
     }
 }
 
