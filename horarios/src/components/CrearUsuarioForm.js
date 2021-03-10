256a099d3c896b2c115c2cf507ca426cbde19557
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-10 15:04:10 -0300

    fix typo

--- horarios/src/components/CrearUsuarioForm.js
+++ horarios/src/components/CrearUsuarioForm.js
@@ -43,10 +43,10 @@ export default class CrearUsuarioForm extends Component {
 
     axios(config).then(response => {
       if (response.status === 201) {
-        alert("Se ha creado el usuario correctamente")
+        notify("Se ha creado el usuario correctamente")
         window.location.href = 'http://200.14.84.238:80'
       } else {
-        alert("No se ha podido crear la  cuenta correctamente")
+        notify("No se ha podido crear la  cuenta correctamente")
       }
 
     }).catch(function (error) {
