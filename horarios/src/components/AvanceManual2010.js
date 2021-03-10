0947eab04a88421779c8e418bceadbcb70cf91e7
agamboar <gamboaalvaro96@gmail.com>
2021-03-09 22:47:00 -0300

    fix 2018

--- horarios/src/components/AvanceManual2010.js
+++ horarios/src/components/AvanceManual2010.js
@@ -7,24 +7,6 @@ import { Link } from 'react-router-dom';
 
 
 
-/*
-
-
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}*/
-
 export default class AvanceManual extends Component {
     state = {
         malla: '2010',
--- horarios/src/components/RamoP.js
+++ horarios/src/components/RamoP.js
@@ -3,59 +3,59 @@ import React, { Component } from 'react'
 
 
 export default class RamoE extends Component {
-    
+
     render() {
         console.log(this.props)
-        if(this.props.state===null){
-        return (
-            <div className="col">  
-                    <div className="card border-primary">                  
-                        <h6 className="card-title text-center"><font size="2">{this.props.codigo}</font></h6>
-                        <p className="card-text"><font size="2">                            
-                            &nbsp;&nbsp;° Nombre: {this.props.ramo}
-                            <br/>
-                            <br/>
-                            &nbsp;&nbsp;Ya aprobaste este ramo, Felicidades!!
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
-
-    
-                        </font></p> 
-                    </div>
-                </div>   
-        )
-        }else{
+        if (this.props.state === null) {
             return (
-                <div className="col">  
-                    <div className="card border-primary">                  
+                <div className="col">
+                    <div className="card border-primary">
                         <h6 className="card-title text-center"><font size="2">{this.props.codigo}</font></h6>
-                        <p className="card-text"><font size="2">                            
+                        <p className="card-text"><font size="2">
                             &nbsp;&nbsp;° Nombre: {this.props.ramo}
-                            <br/>
-                            &nbsp;&nbsp;° Inicio Temprano: {this.props.state[0]}
-                            <br/>
-                            &nbsp;&nbsp;° Termino Temprano: {this.props.state[1]}
-                            <br/>
-                            &nbsp;&nbsp;° Inicio Tardio: {this.props.state[2]}
-                            <br/>
-                            &nbsp;&nbsp;° Termino Tardio: {this.props.state[3]}
-                            <br/>
-                            &nbsp;&nbsp;° Holgura: {this.props.state[4]}
-    
-                        </font></p> 
+                            <br />
+                            <br />
+                            &nbsp;&nbsp;Ya aprobaste este ramo, Felicidades!!
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+
+
+                        </font></p>
                     </div>
-                </div>    
+                </div>
+            )
+        } else {
+            return (
+                <div className="col">
+                    <div className="card border-primary">
+                        <h6 className="card-title text-center"><font size="2">{this.props.codigo}</font></h6>
+                        <p className="card-text"><font size="2">
+                            &nbsp;&nbsp;° Nombre: {this.props.ramo}
+                            <br />
+                            &nbsp;&nbsp;° Inicio Temprano: {this.props.state[0]}
+                            <br />
+                            &nbsp;&nbsp;° Termino Temprano: {this.props.state[2]}
+                            <br />
+                            &nbsp;&nbsp;° Inicio Tardio: {this.props.state[1]}
+                            <br />
+                            &nbsp;&nbsp;° Termino Tardio: {this.props.state[3]}
+                            <br />
+                            &nbsp;&nbsp;° Holgura: {this.props.state[4]}
+
+                        </font></p>
+                    </div>
+                </div>
             )
 
         }
     }
 }
 /*
-    Verde       <div className="card border-primary text-center bg-success" >                  
+    Verde       <div className="card border-primary text-center bg-success" >
     Amarillo    <div className="card border-primary text-center bg-warning" >
-    Azul        <div className="card border-primary text-center bg-info" >  
+    Azul        <div className="card border-primary text-center bg-info" >
 
 
 */
