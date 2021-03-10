c3f6b35a76154edc8c69888ef499fa53025c1ced
BenjaminMorales <benjamin.morales1997@gmail.com>
2021-03-10 00:29:42 -0300

    alertas

--- horarios/src/components/UserInterface.js
+++ horarios/src/components/UserInterface.js
@@ -20,23 +20,7 @@ export default class UserInterface extends Component {
                         <h1 className="title text-primary">BIENVENIDO/A AL GENERADOR DE HORARIOS</h1>
                         <br/>
                         <p className="lead">Que tengas una buena toma de ramos!!!</p>
-                    </div>
-                    <button type="button" class="btn btn-primary" id="liveToastBtn">Show live toast</button>
-
-                        <div class="position-fixed bottom-0 right-0 p-3">
-  <div id="liveToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true" data-delay="2000">       
-    <div class="toast-header">
-      <strong class="mr-auto">Bootstrap</strong>
-      <small>11 mins ago</small>
-      <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
-    <div class="toast-body">
-      Hello, world! This is a toast message.
-    </div>
-  </div>
-</div>
+                    </div> 
                 </div>
                 
 
