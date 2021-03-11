e5c8d2265b3098d7ba55fd6bfcc5453bff052803
BenjaminMorales <benjamin.morales1997@gmail.com>
2021-03-10 21:06:29 -0300

    correcion 3

---
+++ horarios/src/components/M2020Extra5.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import Navbar from './Navbar'
+import Malla2020Extra5  from './Malla2020Extra5'
+
+
+
+export default class M2020Extra1 extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar/>
+                
+                <Malla2020Extra5/>
+
+                
+                
+            </div>
+        )
+    }
+}
