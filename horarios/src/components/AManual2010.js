a39df61a84fc7e60e1f78f5bad9f925cf0bf7955
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-11 01:00:59 -0300

    save avance

--- horarios/src/components/AManual2010.js
+++ horarios/src/components/AManual2010.js
@@ -5,12 +5,36 @@ import AvanceManual2010 from './AvanceManual2010'
 
 
 export default class AManual extends Component {
+
+    state = {
+        ramos_aprobados: null
+    }
+
+    componentDidMount = async () => {
+
+        var config = {
+            method: 'get',
+            url: 'http://200.14.84.238:80/',
+            headers: {
+                'Authorization': 'Token ' + localStorage.getItem("token"), //cambiiar a localStorage
+                'Content-Type': 'application/json'
+            }
+        };
+
+        var ramos_aprobados= await axios(config)
+
+
+        this.setState({
+            ramos: ramos_aprobados.data
+        })
+    }
+
     render() {
         return (
             <div>
                 <Navbar/>
                 
-                <AvanceManual2010/>
+                <AvanceManual2010 ramos={this.state.ramos_aprobados}/>
 
                 
                 
