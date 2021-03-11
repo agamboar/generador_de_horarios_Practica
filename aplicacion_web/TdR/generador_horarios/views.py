b80149195a98e596379e0d97d342b98ba471e009
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-11 17:46:13 -0300

    username and dropdown navbar

--- aplicacion_web/TdR/generador_horarios/views.py
+++ aplicacion_web/TdR/generador_horarios/views.py
@@ -533,7 +533,7 @@ def get_asignaturas_cursadas(request):
 def set_staff(request):
     if request.method == "POST":
         current_user = request.user.id
-        if User.objects.get(id=current_user.id).is_staff:
+        if User.objects.get(id=current_user).is_staff:
             aux_new_staff=User.objects.get(username=request.data.username)
             aux_new_staff.is_staff = True
             aux_new_staff.save()
