a531d3763551df00926549725e3cc2ad26c6a9b0
BenjaminMorales <benjamin.morales1997@gmail.com>
2021-03-10 22:23:41 -0300

    correccion 5

--- horarios/src/components/PERT.js
+++ horarios/src/components/PERT.js
@@ -63,7 +63,7 @@ export default class PERT extends Component {
                                 <div className = "align-self-center">
                                     <button type="submit" className="btn btn-secondary rounded-pill btn-sm" onClick = {this.onSubmit}>
                                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                    <font size="3">Priorizar Ramos</font>
+                                    Priorizar Ramos
                                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                                     </button>
                                 </div>
