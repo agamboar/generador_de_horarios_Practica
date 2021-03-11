7b6030e796ee07a4f3257cafe929571a98edea62
BenjaminMorales <benjamin.morales1997@gmail.com>
2021-03-10 22:36:30 -0300

    correccion 5

--- horarios/src/components/Login.js
+++ horarios/src/components/Login.js
@@ -113,7 +113,13 @@ export default class GoogleSocialAuth extends Component {
             return (
                 <div className="col-md-4 offset-md-4 align-self-sm-end">
                     <div className="card card-body border border-primary shadow-lg p-3 mb-5 bg-white rounded">
-                        <h4 className=" align-self-center">Inicio de Sesión</h4>
+                    <h2 className="text-center " >
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+              <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+              <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+            </svg>
+                &nbsp;Inicio de Sesión
+              </h2>
                         <br />
                         <h5>
                             &nbsp;&nbsp;&nbsp;
