8086a994040aa47a37307db59ccbed7e7e4d5694
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-11 16:35:20 -0300

    username and dropdown navbar

--- horarios/src/components/Navbar.js
+++ horarios/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
-import  { Dropdown ,DropdownButton } from 'react-bootstrap/Button';
+import  { Dropdown ,DropdownButton } from 'react-bootstrap';
 
 export default class Navbar extends Component {
     deleteToken = (e) => {
