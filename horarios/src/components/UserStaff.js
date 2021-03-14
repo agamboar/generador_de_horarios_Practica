547b241626cdf63c9c285fbfcfbf2eb0b9a3679f
BenjaminMorales <benjamin.morales1997@gmail.com>
2021-03-13 22:35:07 -0300

    usuarios

---
+++ horarios/src/components/UserStaff.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+
+
+export default class RamoE extends Component {
+
+    render() {
+        if (this.props.staff === true) {
+            return (
+                <div className = "card border-primary shadow-lg p-3 mb-5 bg-white"> 
+                    Nombre Usuario: ejemplo 
+                    <br/>
+                    Usuario Staff: true
+                    <button type="button" class="btn btn-danger"  > Eliminar de Staff</button>
+                </div>
+            )
+
+        }else if(this.props.staff===false){
+
+            return (
+                <div className = "card border-primary shadow-lg p-3 mb-5 bg-white"> 
+                    Nombre Usuario: ejemplo 
+                    <br/>
+                    Usuario Staff: false
+                    <button type="button" class="btn btn-success"  > Agregarlo a Staff</button>
+                </div>
+            )
+        }else{
+            return (
+                <div/>
+            )
+        }
+    }
+}
