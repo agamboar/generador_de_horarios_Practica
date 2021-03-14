5faf5ea55147aac2fbfafff654d2ee0b2ca1b016
agamboar <gamboaalvaro96@gmail.com>
2021-03-14 18:41:52 -0300

    staff usuarios

--- horarios/src/components/Usuarios.js
+++ horarios/src/components/Usuarios.js
@@ -54,9 +54,9 @@ export default class CrearHorario extends Component {
           }).catch(function (error) {
             if (error.response) {
       
-              if (error.response.data.noUser){ notify(`No User:  ${error.response.data.noUser[0]}`); }
-              if (error.response.data.notStaff) { notify(`Not Staff:  ${error.response.data.notStaff[0]}`); }
-              if (error.response.data.unauthorized) { notify(`Not Authorized:  ${error.response.data.unauthorized[0]}`);}
+              if (error.response.data.noUser){ notify(`No User:  ${error.response.data.noUser}`); }
+              if (error.response.data.isStaff) { notify(`Already Staff:  ${error.response.data.isStaff}`); }
+              if (error.response.data.unauthorized) { notify(`Not Authorized:  ${error.response.data.unauthorized}`);}
               console.log(error.response.data);
             }
           });
@@ -96,9 +96,9 @@ export default class CrearHorario extends Component {
           }).catch(function (error) {
             if (error.response) {
       
-              if (error.response.data.noUser){ notify(`No User:  ${error.response.data.noUser[0]}`); }
-              if (error.response.data.notStaff) { notify(`Not Staff:  ${error.response.data.notStaff[0]}`); }
-              if (error.response.data.unauthorized) { notify(`Not Authorized:  ${error.response.data.unauthorized[0]}`);}
+              if (error.response.data.noUser){ notify(`No User:  ${error.response.data.noUser}`); }
+              if (error.response.data.notStaff) { notify(`Not Staff:  ${error.response.data.notStaff}`); }
+              if (error.response.data.unauthorized) { notify(`Not Authorized:  ${error.response.data.unauthorized}`);}
               console.log(error.response.data);
             }
           });
