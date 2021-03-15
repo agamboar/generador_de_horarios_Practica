e22d0ec353862bcea7a1d63395ca2d8942a361e5
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-15 03:37:59 -0300

    redirect priorizar

--- horarios/src/components/Horarios.js
+++ horarios/src/components/Horarios.js
@@ -8,7 +8,7 @@ class Horarios extends Component {
     render() {
         console.log(this.props.horarios  )
         if (this.props.horarios == "n"){
-            return(<Redirect to="/users/usr/crearHorario" />)
+            setTimeout(function () { return( <Redirect to="/users/usr/crearHorario" />)}, 5000);
         }else{
             return this.props.horarios.map((solucion, index) => (solucion)? <Horario horario={solucion} key={index} index={index}/>:null)}
         
