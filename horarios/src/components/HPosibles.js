6bf22be0cad57c0faf16f3883d012bb9e0b72880
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-15 03:04:25 -0300

    redirect priorizar

--- horarios/src/components/HPosibles.js
+++ horarios/src/components/HPosibles.js
@@ -8,7 +8,7 @@ import { Redirect } from 'react-router';
 export default class HPosibles extends Component {
     aux =()=>{
         //console.log("si")
-        return(<Redirect to="/users/usr/crearHorario" />)
+        setTimeout(function () { return(<Redirect to="/users/usr/crearHorario" />)}, 5000)
         //setTimeout(function () { window.location.href = 'http://200.14.84.238:80/users/usr/crearHorario' }, 5000);
     } 
     state = {
--- horarios/src/components/Horarios.js
+++ horarios/src/components/Horarios.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 
 import Horario from './Horario.js';
+import { Redirect } from 'react-router';
 
 class Horarios extends Component {
 
     render() {
         console.log(this.props.horarios  )
         if (this.props.horarios == "n"){
-            return null
+            return(<Redirect to="/users/usr/crearHorario" />)
         }else{
             return this.props.horarios.map((solucion, index) => (solucion)? <Horario horario={solucion} key={index} index={index}/>:null)}
         
