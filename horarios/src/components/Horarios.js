e2dc12834ce657746a16f169012cece57f11aaa4
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-23 02:46:48 -0300

    fix error front generacion horarios

--- horarios/src/components/Horarios.js
+++ horarios/src/components/Horarios.js
@@ -10,6 +10,7 @@ class Horarios extends Component {
         if (this.props.horarios == "n"){
             setTimeout(function () { return( <Redirect to="/users/usr/PERT" />)}, 5000);
         }else if (this.props.horarios != null){
+            console.log(this.props.horarios)
             return this.props.horarios.map((solucion, index) => (solucion)? <Horario horario={solucion} key={index} index={index}/>:<div>hola</div>)
         }else{
             return <div>adios</div>
