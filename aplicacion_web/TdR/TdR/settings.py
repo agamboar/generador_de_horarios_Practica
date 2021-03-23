7236ee3766f74914c275092499a9f1daf484d9c0
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-23 02:17:11 -0300

    static file in django

--- aplicacion_web/TdR/TdR/settings.py
+++ aplicacion_web/TdR/TdR/settings.py
@@ -68,7 +68,7 @@ SESSION_COOKIE_AGE = 10000
 FILE_UPLOAD_HANDLERS = ("django_excel.ExcelMemoryFileUploadHandler",
                         "django_excel.TemporaryExcelFileUploadHandler")
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','200.14.84.238']
 CSRF_TRUSTED_ORIGINS = ['127.0.0.1', 'localhost', "http://200.14.84.238:80/"]
 
 
