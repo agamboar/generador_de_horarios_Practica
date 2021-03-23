44fdda0458fc37db965966c9aadeee43a5eaa374
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-23 02:25:42 -0300

    static file in django

--- aplicacion_web/TdR/TdR/settings.py
+++ aplicacion_web/TdR/TdR/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
 
-
+print(BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
