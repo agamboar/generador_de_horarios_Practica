836e0efd25d422465724e0f9dd86409e3d307b2f
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-23 16:09:25 -0300

    django to apache

--- aplicacion_web/TdR/TdR/wsgi.py
+++ aplicacion_web/TdR/TdR/wsgi.py
@@ -7,10 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/usr/generador_de_horarios_Practica/aplicacion_web/TdR/TdR')
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TdR.settings')
+
+os.environ.setdefault("LANG", "es_ES.UTF-8")
+os.environ.setdefault("LC_ALL", "es_ES.UTF-8")
 
 application = get_wsgi_application()
