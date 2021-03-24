a7fd01ae97f7ec0108ca3506b373ad30e117cbc2
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-23 21:26:44 -0300

    ssl django

--- horarios/src/components/AvanceManual2010.js
+++ horarios/src/components/AvanceManual2010.js
@@ -24,7 +24,7 @@ export default class AvanceManual extends Component {
 
         var config = {
             method: 'get',
-            url: 'http://200.14.84.238:80/asignaturasCursadas/',
+            url: 'asignaturasCursadas/',
             headers: {
                 'Authorization': 'Token ' + localStorage.getItem("token"), //cambiiar a localStorage
                 'Content-Type': 'application/json'
