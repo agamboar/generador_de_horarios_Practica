19944ae568daef94c960106a5d1a60576e40dc4e
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-29 11:00:21 -0300

    fix npm modules

--- aplicacion_web/TdR/TdR/settings.py
+++ aplicacion_web/TdR/TdR/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-print(BASE_DIR.parent().child('horarios'))
+print(BASE_DIR.child('horarios'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
