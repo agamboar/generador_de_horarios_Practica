479da860183d312ca230228767560cbc8fcb890d
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-28 22:00:57 -0300

    fix google

--- horarios/src/components/PriorizarSeccion.js
+++ horarios/src/components/PriorizarSeccion.js
@@ -6,140 +6,6 @@ import { DownOutlined } from '@ant-design/icons';
 
 import axios from 'axios';
 
-
-export default class PriorizarSeccion extends Component {
-
- 
-    render() {
-        return (
-            <div>
-                <Navbar/>     
-                <p>prueba</p>
-            </div>
-        )
-    }
-}
-
-
-
-
-
-
-
-/*
-import { sortableContainer, sortableElement, sortableHandle } from 'react-sortable-hoc';
-import { MenuOutlined } from '@ant-design/icons';
-import arrayMove from 'array-move';
-
-const DragHandle = sortableHandle(() => <MenuOutlined style={{ cursor: 'grab', color: '#999' }} />);
-
-const columns = [
-  {
-    title: 'Sort',
-    dataIndex: 'sort',
-    width: 30,
-    className: 'drag-visible',
-    render: () => <DragHandle />,
-  },
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    className: 'drag-visible',
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-  },
-];
-
-const data = [ //secciones  fetch
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    index: 0,
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    index: 1,
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    index: 2,
-  },
-];
-
-const SortableItem = sortableElement(props => <tr {...props} />);
-const SortableContainer = sortableContainer(props => <tbody {...props} />);
-
-class SortableTable extends React.Component {
-  state = {
-    dataSource: data,
-  };
-
-  onSortEnd = ({ oldIndex, newIndex }) => {
-    const { dataSource } = this.state;
-    if (oldIndex !== newIndex) {
-      const newData = arrayMove([].concat(dataSource), oldIndex, newIndex).filter(el => !!el);
-      console.log('Sorted items: ', newData);
-      this.setState({ dataSource: newData });
-    }
-  };
-
-  DraggableContainer = props => (
-    <SortableContainer
-      useDragHandle
-      disableAutoscroll
-      helperClass="row-dragging"
-      onSortEnd={this.onSortEnd}
-      {...props}
-    />
-  );
-
-  DraggableBodyRow = ({ className, style, ...restProps }) => {
-    const { dataSource } = this.state;
-    // function findIndex base on Table rowKey props and should always be a right array index
-    const index = dataSource.findIndex(x => x.index === restProps['data-row-key']);
-    return <SortableItem index={index} {...restProps} />;
-  };
-
-  render() {
-    const { dataSource } = this.state;
-
-    return (
-        <div>
-            <p>Solo se muestran las secciones disponibles segun nuestros registros, pueden variar en el portal</p>
-            <p>Los cupos de las secciones mostradas pueden estar desactulizadas</p>
-            <br></br>
-            
-            <Table
-                pagination={false}
-                dataSource={dataSource}
-                columns={columns}
-                rowKey="index"
-                components={{
-                body: {
-                    wrapper: this.DraggableContainer,
-                    row: this.DraggableBodyRow,
-                },
-                }}
-            />
-      </div>
-    );
-  }
-}
-
 import { Input, AutoComplete } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 
@@ -191,16 +57,31 @@ const options = [
   },
 ];
 
-const Complete = () => (
-  <AutoComplete
-    dropdownClassName="certain-category-search-dropdown"
-    dropdownMatchSelectWidth={500}
-    style={{
-      width: 250,
-    }}
-    options={options}
-  >
-    <Input.Search size="large" placeholder="input here" />
-  </AutoComplete>
-);
-*/
+
+export default class PriorizarSeccion extends Component {
+
+ 
+    render() {
+        return (
+            <div>
+                <Navbar/>     
+                
+                <AutoComplete
+                  dropdownClassName="certain-category-search-dropdown"
+                  dropdownMatchSelectWidth={500}
+                  style={{
+                    width: 250,
+                  }}
+                  options={options}
+                >
+                  <Input.Search size="large" placeholder="input here" />
+              </AutoComplete>
+
+                
+            </div>
+        )
+    }
+}
+
+
+
