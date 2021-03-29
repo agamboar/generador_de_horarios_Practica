10da208274808f3223943be358fe8543a9e953f1
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-29 11:04:57 -0300

    fix google

--- aplicacion_web/TdR/TdR/settings.py
+++ aplicacion_web/TdR/TdR/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-print(str(Path(__file__).resolve().parent.parent.parent / 'horarios'))
+print(str(Path(__file__).resolve().parent.parent.parent / 'horarios/node_modules'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
