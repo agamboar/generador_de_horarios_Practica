d421fc41237f076235fc989e94f34447057cc663
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-28 22:02:52 -0300

    fix get secciones

--- horarios/src/components/PriorizarSeccion.js
+++ horarios/src/components/PriorizarSeccion.js
@@ -9,59 +9,58 @@ import axios from 'axios';
 import { Input, AutoComplete } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 
-const renderTitle = (title) => (
-  <span>
-    {title}
-    <a
-      style={{
-        float: 'right',
-      }}
-      href="https://www.google.com/search?q=antd"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      more
-    </a>
-  </span>
-);
-
-const renderItem = (title, count) => ({
-  value: title,
-  label: (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'space-between',
-      }}
-    >
-      {title}
-      <span>
-        <UserOutlined /> {count}
-      </span>
-    </div>
-  ),
-});
-
-const options = [
-  {
-    label: renderTitle('Libraries'),
-    options: [renderItem('AntDesign', 10000), renderItem('AntDesign UI', 10600)],
-  },
-  {
-    label: renderTitle('Solutions'),
-    options: [renderItem('AntDesign UI FAQ', 60100), renderItem('AntDesign FAQ', 30010)],
-  },
-  {
-    label: renderTitle('Articles'),
-    options: [renderItem('AntDesign design language', 100000)],
-  },
-];
 
 
 export default class PriorizarSeccion extends Component {
-
- 
     render() {
+      const renderTitle = (title) => (
+        <span>
+          {title}
+          <a
+            style={{
+              float: 'right',
+            }}
+            href="https://www.google.com/search?q=antd"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            more
+          </a>
+        </span>
+      );
+      
+      const renderItem = (title, count) => ({
+        value: title,
+        label: (
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+            }}
+          >
+            {title}
+            <span>
+              <UserOutlined /> {count}
+            </span>
+          </div>
+        ),
+      });
+      
+      const options = [
+        {
+          label: renderTitle('Libraries'),
+          options: [renderItem('AntDesign', 10000), renderItem('AntDesign UI', 10600)],
+        },
+        {
+          label: renderTitle('Solutions'),
+          options: [renderItem('AntDesign UI FAQ', 60100), renderItem('AntDesign FAQ', 30010)],
+        },
+        {
+          label: renderTitle('Articles'),
+          options: [renderItem('AntDesign design language', 100000)],
+        },
+      ];
+      
         return (
             <div>
                 <Navbar/>     
