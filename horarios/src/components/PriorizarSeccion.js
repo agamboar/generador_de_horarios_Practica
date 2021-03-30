769aeedf88eae5b4b1d850d103ead3a708728729
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-30 14:40:40 -0300

    fix prio seccion

--- horarios/src/components/PriorizarSeccion.js
+++ horarios/src/components/PriorizarSeccion.js
@@ -91,6 +91,7 @@ export default class UserInterface extends Component {
                       <div>
                       <div style={{padding: 10, display: "flex",  justifyContent: "center"}}><Alert message="(Deje las secciones que más le importen al inicio de la tabla)" type="warning" /></div>
                       <Card title={"Secciones del ramo: \""+this.state.message["nombre"]+"\""}>
+                        {console.log(this.state.message["secciones"])}
                         <TablaSecciones secciones= {this.state.message["secciones"]}/> {/*aca pasar el codigo del ramo y dps solo recibir el arreglo para mandarlo a la base*/}
                       </Card>
                       </div>
