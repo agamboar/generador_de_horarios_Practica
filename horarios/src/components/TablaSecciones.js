c0d78ceb466c14af114af959f7c18d908df76215
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-30 14:52:23 -0300

    fix prio seccion

--- horarios/src/components/TablaSecciones.js
+++ horarios/src/components/TablaSecciones.js
@@ -117,7 +117,7 @@ class SortableTable extends React.Component {
   render() {
     const { dataSource } = this.state;
     this.props?console.log(this.props):console.log("nada")
-
+    this.setState({dataSource:this.props.secciones})
     return (
       <Table
         pagination={false}
