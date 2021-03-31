c2f293577ba94cb446e7f54a0b1f1a837950da1b
NigusFs <nicolas.floress@mail.udp.cl>
2021-03-31 16:37:03 -0300

    fix get secciones add id

--- aplicacion_web/TdR/generador_horarios/views.py
+++ aplicacion_web/TdR/generador_horarios/views.py
@@ -660,7 +660,7 @@ def get_secciones_disponibles(request, codigo):
 
             else:
                 if cod_sec != "99":
-                    aux_retornar.append({'cod_seccion':cod_sec, 'numb_seccion':numb_seccion,'profesor':prof,'vac_libres':vac_libres,'horario': aux_horario,'index':index  })
+                    aux_retornar.append({'id':elem['id'],'cod_seccion':cod_sec, 'numb_seccion':numb_seccion,'profesor':prof,'vac_libres':vac_libres,'horario': aux_horario,'index':index  })
                     index+=1
                 aux_horario = []
                 aux_horario.append(horario)
