b4a45336bd13a4370b10829fba840deaab5f28bd
NigusFs <nicolas.floress@mail.udp.cl>
2021-04-01 20:54:53 -0300

    add button

--- horarios/src/components/PriorizarSeccion.js
+++ horarios/src/components/PriorizarSeccion.js
@@ -76,9 +76,9 @@ export default class UserInterface extends Component {
                   <Space direction="vertical">
                   <Row>
                     <Col flex="auto"><Title style={{ color: "#007bff"}}level={1}>Priorización de secciones</Title></Col>    
-                    <Col flex="auto"><Title  style={{ marginLeft: 35}} level={5}>Ir a:</Title> </Col>
-                    <Col flex="right"><Link className="nav-link" to={{ pathname: '/users/usr/priorizarRamos' }} ><Button type="primary">Priorizar Ramos</Button></Link></Col>
-                    <Col flex="right"><Link className="nav-link" to={{ pathname: '/users/usr/horariosPosibles' }}  ><Button  type="primary">Generar Horario</Button></Link></Col>
+                    
+                    <Col flex="auto"><Link className="nav-link" to={{ pathname: '/users/usr/priorizarRamos' }} ><Button color="#616161">Priorizar Ramos</Button></Link></Col>
+                    <Col flex="auto"><div style={{padding: 10, display: "flex",  justifyContent: "flex-end"}} ><Link className="nav-link" to={{ pathname: '/users/usr/horariosPosibles' }} ><Button  color="#616161">Generar Horario</Button></Link></div></Col>
                   </Row>
                   <Title  style={{ marginLeft: 35}} level={4}>Escoja un ramo para que pueda priorizar sus secciones</Title>
                   </Space>
