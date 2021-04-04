2b2a262fa939b28f4ab3101538a34c5372a75541
NigusFs <nicolas.floress@mail.udp.cl>
2021-04-04 18:25:27 -0400

    fix get nodo seccion

--- aplicacion_web/TdR/generador_horarios/views.py
+++ aplicacion_web/TdR/generador_horarios/views.py
@@ -668,7 +668,6 @@ def get_secciones_disponibles(request, codigo): #revisar esta funcion, saca bien
                         aux_retornar.append({'id':id_nodo_seccion,'cod_seccion':cod_sec, 'numb_seccion':numb_seccion,'profesor':prof,'vac_libres':vac_libres,'horario': aux_horario,'index':index, 'ss':ss_nodo_seccion  })
                         index+=1
                         aux_horario = []
-                        aux_horario.append(horario)
                         prof = ""
                         #aux_codigo_sec = elem['to_seccion__cod_seccion']
 
