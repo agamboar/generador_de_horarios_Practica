02ea52ab03bb1f82edd2f63686c859404da1a0f6
thomas.gonzalez@mail.udp.cl <thomas.gonzalez@mail.udp.cl>
2021-07-23 16:39:56 -0400

    funcion para reconocer area de cfg

---
+++ aplicacion_web/TdR/generador_horarios/clique_algorithm/readme.txt
@@ -0,0 +1,7 @@
+clique_actual.py es la ultima version del calculo de recomendaciones y la que se utiliza en views.py. ajustes y nuevas funciones deben hacerse en este archivo.
+
+en caso de necesitar refactorizar clique_actual.py, recomiendo el siguiente flujo:
+    renombrar clique_actual.py -> clique_v3.py
+    crear copia de clique_v3.py -> clique_v3 copy.py
+    renombrar clique_v3 copy.py -> clique_actual.py
+luego trabajar en clique_actual.py
---
+++ aplicacion_web/TdR/generador_horarios/codigos_cfg/categorize_cfgs.py
@@ -0,0 +1,23 @@
+
+PATH = './generador_horarios/codigos_cfg/codigos_set1/'
+
+class CfgNotFoundException(Exception):
+    def __init__(self, message='No encontro el cfg en archivos areas en '+ PATH):
+        # Call the base class constructor with the parameters it needs
+        super(CfgNotFoundException, self).__init__(message)
+
+def get_codigos(fileName='codigos_Historia.txt') -> list:
+    path = PATH + fileName
+    with open(path, 'r') as f:
+        codigos = f.read().splitlines()
+        return codigos
+
+def get_area(codigo : str) -> str:
+    if codigo in get_codigos('codigos_Historia.txt'): return 'Historia'
+    if codigo in get_codigos('codigos_Ciencia-y-Sociedad.txt'): return 'Ciencia y Sociedad' 
+    if codigo in get_codigos('codigos_Ciencias-Sociales.txt'): return 'Ciencias Sociales'
+    if codigo in get_codigos('codigos_Humanidades.txt'): return 'Humanidades'
+    raise CfgNotFoundException('no se encontro codigo cfg: ' +codigo+ 'en catalogo de areas (' +PATH+ ')')
+    
+
+
