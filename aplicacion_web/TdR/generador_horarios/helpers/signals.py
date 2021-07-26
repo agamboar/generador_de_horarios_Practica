084ac48f5c84a014c334023e7b8b508016fe09db
Thomas-gonzalez <thomas.gonzalez@mail.udp.cl>
2021-07-25 23:25:28 -0400

    Update signals.py

--- aplicacion_web/TdR/generador_horarios/helpers/signals.py
+++ aplicacion_web/TdR/generador_horarios/helpers/signals.py
@@ -4,16 +4,16 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from ..models import alumno
 
-@receiver(post_save, sender=User)
-def create_alumno(sender, instance, created, **kwargs):
-    if created:
-        alumno.objects.create(to_user=instance)
+# @receiver(post_save, sender=User)
+# def create_alumno(sender, instance, created, **kwargs):
+#     if created:
+#         alumno.objects.create(to_user=instance)
 
 
-@receiver(post_save, sender=User)
-def save_alumno(sender, instance, **kwargs):
-    to_alumno = instance.alumno
-    if to_alumno:
-        to_alumno.save()
+# @receiver(post_save, sender=User)
+# def save_alumno(sender, instance, **kwargs):
+#     to_alumno = instance.alumno
+#     if to_alumno:
+#         to_alumno.save()
     
 
